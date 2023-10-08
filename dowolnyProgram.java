7d04fb028b24dc2d92cf1b197e8464e6a9ff4582
Ewelina Kostępska <kostepska@outlook.com>
2023-10-08 16:21:23 +0200

    Add files via upload

---
+++ dowolnyProgram.java
@@ -0,0 +1,11 @@
+public class dowolnyProgram {
+    public static void main(String[] args) {
+        int liczba = 42;
+        double liczbaZmiennoprzecinkowa = 3.14;
+        String tekst = "Hello World!";
+
+        System.out.println("Liczba: " + liczba);
+        System.out.println("Liczba zmiennoprzecinkowa: " + liczbaZmiennoprzecinkowa);
+        System.out.println("Tekst: " + tekst);
+    }
+}
